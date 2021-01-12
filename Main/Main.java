7d3c6f769c12f5594799fcd65c21b2b2a80e862c
Appletee <shakedgold2005@gmail.com>
2021-01-12 22:31:47 +0200

    Update Main.java

--- Main/Main.java
+++ Main/Main.java
@@ -2,23 +2,38 @@ package Main;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.BorderFactory;
+
+import java.awt.BorderLayout;
+
+
 import java.awt.Color;
 
 public class Main extends JFrame {
-    private static void settings(int width, int height) {
+    /**
+     *there was an error. this fixes idk why
+     */
+    private static final long serialVersionUID = -7379587371723737961L;
+
+    private static JFrame settings(int width, int height) {
         JFrame frame = new JFrame("Calculator");
         JPanel screen = new JPanel();
-        
-        frame.setSize(width, height);
-        frame.add(screen);
+
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        frame.setSize(width, height);
+        frame.getContentPane().add(screen, BorderLayout.CENTER);    
 		frame.setBackground(Color.WHITE);
 		frame.setTitle("Calculator");
-		frame.setLocationRelativeTo(null);
+        return frame;
+        
+    }
+    private static void start(JFrame frame) {
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        settings(400, 600);
+        start(settings(400, 600));
     }
 }
