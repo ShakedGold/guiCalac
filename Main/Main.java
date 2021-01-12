ea600b996f762b76f9e59eb33319ae2b6f84f22f
Appletee <shakedgold2005@gmail.com>
2021-01-12 22:51:26 +0200

    Update Main.java

--- Main/Main.java
+++ Main/Main.java
@@ -1,28 +1,37 @@
 package Main;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.Border;
 import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 
-import java.awt.BorderLayout;
-
-
+import java.awt.BasicStroke;
 import java.awt.Color;
 
 public class Main extends JFrame {
     /**
-     *there was an error. this fixes idk why
+     * There was an error. This fixes idk why
      */
     private static final long serialVersionUID = -7379587371723737961L;
 
-    private static JFrame settings(int width, int height) {
+    private static JFrame settings(int width, int height, int pHeight) {
         JFrame frame = new JFrame("Calculator");
         JPanel screen = new JPanel();
+        JPanel buttons = new JPanel();
+        JLabel label = new JLabel("Click The Numbers To Calculate");
+
+        label.setBounds(0, 600, screen.getWidth(), screen.getHeight());
+
+        screen.setBounds(0,0,width - 15, pHeight);
+        screen.setBorder(BorderFactory.createStrokeBorder(new BasicStroke(4.2f)));
+        buttons.setBounds(600 - height, width, screen.getWidth(), screen.getHeight());
+        buttons.setBorder(BorderFactory.createStrokeBorder(new BasicStroke(4.2f)));
 
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.setSize(width, height);
-        frame.getContentPane().add(screen, BorderLayout.CENTER);    
+        frame.getContentPane().add(screen);
+        frame.getContentPane().add(buttons);
 		frame.setBackground(Color.WHITE);
 		frame.setTitle("Calculator");
         return frame;
@@ -34,6 +43,6 @@ public class Main extends JFrame {
     }
 
     public static void main(String[] args) {
-        start(settings(400, 600));
+        start(settings(400, 600, 150));
     }
 }
